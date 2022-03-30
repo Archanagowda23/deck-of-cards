973a3ae70ed95e460d768189bb6febc1aa6b0419
Archanagowda23 <archanar249@gmail.com>
2022-03-30 14:39:16 +0530

    ADDED: functionality to add 4 players to play the game

--- src/com/bridgelabz/deckofcards/DeckOfCards.java
+++ src/com/bridgelabz/deckofcards/DeckOfCards.java
@@ -1,8 +1,34 @@
 package com.bridgelabz.deckofcards;
 
+import java.util.Random;
+
 public class DeckOfCards {
 
     int[][] cards = new int[4][13];
+
+    public void distributeOneCard(int playerNumber) {
+        Random random = new Random();
+
+        int suit = random.nextInt(4);
+        int rank = random.nextInt(13);
+
+        if (cards[suit][rank] != 0) {
+            distributeOneCard(playerNumber);
+        } else {
+            cards[suit][rank] = playerNumber;
+        }
+    }
+
+    /*
+     * distributing cards to each 9 players
+     */
+    public void distributeCards(int playerNumber) {
+        for (int i = 0; i < 9; i++) {
+            distributeOneCard(playerNumber);
+        }
+    }
+
+
 
     String getRank(int rankNumber) {
         switch (rankNumber) {
@@ -56,5 +82,11 @@ public class DeckOfCards {
     public static void main(String[] args) {
         System.out.println("Welcome to the Deck of Cards Game");
         DeckOfCards deckOfCards = new DeckOfCards();
+
+        for (int playerNumber = 1; playerNumber <= 4; playerNumber++) {
+            deckOfCards.distributeCards(playerNumber);
+            System.out.println("Player" + playerNumber + "'s cards :");
+
+        }
     }
 }
